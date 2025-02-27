08da77dc57b4e87c9f1437f775676c9b5324e50e
Kingsley Obinna <obiking797@gmail.com>
2025-02-27 15:36:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var btn=document.querySelector(".btn");
 var menu=document.querySelector(".menu");           
-Tabnine|Edit|Test|Explain|Document
 btn.addEventListener("click",function(){
     menu.classList.toggle("active");
 });
-var navLink=document.querySelectorAll("ul li a");
-for(var i=0;i<navLink.length;i++){
-    Tabnine|Edit|Test|Explain|Document
-    navLink[i].addEventListener("click",function(){
+var navlinks=document.querySelectorAll("ul li a");
+for(var i=0;i<navlinks.length;i++){
+    navlinks[i].addEventListener("click",function(){
         menu.classList.remove("active");
-    });
+    })
 }
